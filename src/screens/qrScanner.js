77ece918e4e476e07388b827e86612a6848a7bf8
Hung Phan <hungphan@Hungs-Mac-mini.local>
2019-05-31 21:00:33 -0700

    change rooms' name

--- src/screens/qrScanner.js
+++ src/screens/qrScanner.js
@@ -19,17 +19,20 @@ export default class QrScanner extends Component {
         console.log(user);
         let phoneNumber = user.email.split('@')[0];
 
-            if(e.data == 'roomA-start' || e.data == 'roomA-end') {
-                this.updateDoctorLocation(phoneNumber, "Room A");
+            if(e.data == 'wr-start' || e.data == 'wr-end') {
+                this.updateDoctorLocation(phoneNumber, "Waiting Room");
             }
-            else if (e.data == 'roomB-start' || e.data == 'roomB-end') {
-                this.updateDoctorLocation(phoneNumber, "Room B");
+            else if (e.data == 'er-start' || e.data == 'er-end') {
+                this.updateDoctorLocation(phoneNumber, "Exam Room");
             }
-            else if (e.data == 'roomC-start' || e.data == 'roomC-end') {
-                this.updateDoctorLocation(phoneNumber, "Room C");
+            else if (e.data == 'tr1-start' || e.data == 'tr1-end') {
+                this.updateDoctorLocation(phoneNumber, "Treatment Room 1");
             }
-            else if (e.data == 'roomD-start' || e.data == 'roomD-end') {
-                this.updateDoctorLocation(phoneNumber, "Room D");
+            else if (e.data == 'tr2-start' || e.data == 'tr2-end') {
+                this.updateDoctorLocation(phoneNumber, "Treatment Room 2");
+            }
+            else if (e.data == 'ct-start' || e.data == 'ct-end') {
+                this.updateDoctorLocation(phoneNumber, "CT Room");
             }
             else if (e.data == 'private') {
                 this.updateDoctorLocation(phoneNumber, "private");

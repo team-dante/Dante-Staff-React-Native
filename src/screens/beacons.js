cec000a4d026891cac54a4d9490f86972e5614de
Xinhao Liang <xinhao128@hotmail.com>
2019-07-08 00:22:44 -0700

    Do not account beacons that are not being detected

--- src/screens/beacons.js
+++ src/screens/beacons.js
@@ -22,7 +22,9 @@ export default class beacons extends Component {
       dataSource: ds.cloneWithRows([]),
       count: 0,
       currRoom: '',
-      roomDict: {}
+      major1: [],
+      major2: [],
+      major3: []
     };
   }
 
@@ -48,6 +50,8 @@ export default class beacons extends Component {
     const majorToRoom = { 1: "exam1", 2: "CTRoom", 3: "femaleWaitingRoom" };
     // map beacon major to its corresponding cutoff value (1m)
     const cutoff = { 1: 1, 2: 1, 3: 1};
+    // after 10 rounds, perform stats analysis
+    const threshold = 10;
 
     // get first part before @email.com
     let user = firebase.auth().currentUser;
@@ -69,26 +73,39 @@ export default class beacons extends Component {
         });
 
         let localCount = this.state.count;
-        if (localCount < 10) {
+        if (localCount < threshold) {
           localCount++;
           this.setState({count: localCount});
-        }
-        // push beacon distance; if undefined or -1, push 0
-        for (let beacon of data.beacons) {
-          if (beacon["accuracy"] != undefined || beacon["accuracy"] != -1)
-            roomDict[ beacon["major"] ].push(beacon["accuracy"])
-          else
-            roomDict[ beacon["major"] ].push(0)
-        }
+          // push beacon distance; if undefined or -1, push 0
+          for (let beacon of data.beacons) {
 
-        // after 10 rounds, implement FIFO
-        if (localCount >= 10) {
+            // debugging purposes
+            if (beacon["major"] == 1) {
+              this.setState({major1: roomDict[beacon["major"]]})
+            } 
+            else if (beacon["major"] == 2) {
+              this.setState({major2: roomDict[beacon["major"]]})
+            }
+            else if (beacon["major"] == 3) {
+              this.setState({major3: roomDict[beacon["major"]]})
+            } 
+            // undefined or -1 will skew the distance to 999m
+            if (beacon["accuracy"] != undefined && beacon["accuracy"] != -1)
+              roomDict[ beacon["major"] ].push(beacon["accuracy"])
+            else
+              roomDict[ beacon["major"] ].push(999)
+          }
+        }
+        // after 10 rounds
+        else {
           let avgList = []
           for (let beacon of data.beacons) {
-            // pop out the oldest beacon data at front of the array
-            roomDict[ beacon["major"] ].shift()
-            const avg = (roomDict[ beacon["major"] ].reduce((acc, c) => acc + c, 0)) / 10.0;
-            avgList.push([ beacon["major"], avg ])
+            // if there are less than 10 collected distances in the array, ignore it (aka. not detected at all)
+            if (roomDict[ beacon["major"] ].length >= threshold) {
+              const total = (roomDict[ beacon["major"] ].reduce((acc, c) => acc + c, 0));
+              avgList.push([ beacon["major"], total/(threshold * 1.0)])
+            }
+            roomDict[ beacon["major"] ] = []
           }
           // avgList = [[major, avg distance], ...] (e.g. avgList = [[1, 1.0], [2, 1.4], [3, 2.0]])
           // sort by avg distance (closest beacon will come first)
@@ -101,14 +118,15 @@ export default class beacons extends Component {
             this.setState({currRoom: 'Private'});
           else
             this.setState({currRoom: majorToRoom[avgList[0][0]]})
+
+          this.updateDoctorLocation(phoneNumber, this.state.currRoom);
+          this.setState({count: 0})
         }
 
         this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(data.beacons),
-          roomDict: roomDict
+          dataSource: this.state.dataSource.cloneWithRows(data.beacons)
         });
         // update the room val to Firebase; change dots on the map
-        this.updateDoctorLocation(phoneNumber, this.state.currRoom);
       }
     );
   }
@@ -149,14 +167,31 @@ export default class beacons extends Component {
   }
 
   render() {
-    const { bluetoothState, dataSource, count, currRoom, roomDict } =  this.state;
+    const { dataSource, count, currRoom, major1, major2, major3 } =  this.state;
 
     return (
       <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.headline}>
           All beacons in the area will be displayed
         </Text>
-        
+        <Text style={{fontWeight: 'bold'}}>Major 1:</Text>
+        {
+          major1.map((item, key)=>(
+            <Text key={key}> {item.toFixed(2)} </Text>
+          ))
+        }
+        <Text style={{fontWeight: 'bold'}}>Major 2:</Text>
+        {
+          major2.map((item, key)=>(
+            <Text key={key}> {item.toFixed(2)} </Text>
+          ))
+        }
+        <Text style={{fontWeight: 'bold'}}>Major 3:</Text>
+        {
+          major3.map((item, key)=>(
+            <Text key={key}> {item.toFixed(2)} </Text>
+          ))
+        }
         <Text style={{fontWeight: 'bold'}}>Count: {count} </Text> 
         <Text style={{fontWeight: 'bold', color: 'blue'}}> You are now in Room {currRoom} </Text>
         <ListView

cda1e8c66a4eb81ae9d0ac21f7a98f39ad9c8599
Xinhao Liang <xinhao128@hotmail.com>
2019-07-07 21:46:37 -0700

    Implemented a queue to track doctor's current location

--- src/screens/beacons.js
+++ src/screens/beacons.js
@@ -5,14 +5,14 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 import firebase from 'firebase';
 import { ScrollView } from 'react-native-gesture-handler';
 
-type Props = {};
-export default class beacons extends Component<Props> {
+export default class beacons extends Component {
   constructor(props) {
     super(props);
     // Create our dataSource which will be displayed in the ListView
     var ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2 }
     );
+    
     this.state = {
       bluetoothState: '',
       // region information
@@ -20,21 +20,13 @@ export default class beacons extends Component<Props> {
       uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e',
       // React Native ListView datasource initialization
       dataSource: ds.cloneWithRows([]),
-      major1: [],
-      major2: [],
-      major3: [],
-      total1: 0,
-      total2: 0,
-      total3: 0,
-      average1: 0,
-      average2: 0,
-      average3: 0,
       count: 0,
-      room: ''
+      currRoom: '',
+      roomDict: {}
     };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     // Request for authorization while the app is open
     Beacons.requestWhenInUseAuthorization();
     // Define a region which can be identifier + uuid,
@@ -50,6 +42,11 @@ export default class beacons extends Component<Props> {
   }
 
   componentDidMount() {
+    let roomDict = {1: [], 2: [], 3:[]};
+    // map beacon major to the real clinic room
+    const majorToRoom = { 1: "exam1", 2: "CTRoom", 3: "femaleWaitingRoom" };
+    // map beacon major to its corresponding cutoff value (1m)
+    const cutoff = { 1: 1, 2: 1, 3: 1};
 
     // get first part before @email.com
     let user = firebase.auth().currentUser;
@@ -64,118 +61,59 @@ export default class beacons extends Component<Props> {
       'beaconsDidRange',
       (data) => {
         console.log(data.beacons);
+        
+        // sort all beacons; from nearest distance to the furthest distance
         data.beacons.sort(function(first, second){
-          return first.major - second.minor;
-        })
-        let localCount = this.state.count
-        let localMajor1 = this.state.major1;
-        let localMajor2 = this.state.major2;
-        let localMajor3 = this.state.major3;
-        let localTotal1 = this.state.total1;
-        let localTotal2 = this.state.total2;
-        let localTotal3 = this.state.total3;
+          return first.accuracy - second.accuracy;
+        });
 
+        let localCount = this.state.count;
         if (localCount < 10) {
           localCount++;
+          this.setState({count: localCount});
+        }
 
-          // major 1 is either in element 0 or 1 or 2
-          if (data.beacons[0] !== undefined && data.beacons[0]["major"] == 1 && data.beacons[0]["accuracy"] !== undefined && data.beacons[0]["accuracy"] != -1)
-          {
-            localMajor1.push(data.beacons[0]["accuracy"])
-            localTotal1++;
-          }  
-          else if (data.beacons[1] !== undefined && data.beacons[1]["major"] == 1 && data.beacons[1]["accuracy"] !== undefined && data.beacons[1]["accuracy"] != -1)
-          {
-            localMajor1.push(data.beacons[1]["accuracy"])
-            localTotal1++;
-          }  
-          else if (data.beacons[2] !== undefined && data.beacons[2]["major"] == 1 && data.beacons[2]["accuracy"] !== undefined && data.beacons[2]["accuracy"] != -1)
-          {
-            localMajor1.push(data.beacons[2]["accuracy"])
-            localTotal1++;
-          } 
-          else
-            localMajor1.push(0)
-
-          // major 2 is either in element 0 or 1 or 2
-          if (data.beacons[0] !== undefined && data.beacons[0]["major"] == 2 && data.beacons[0]["accuracy"] !== undefined && data.beacons[0]["accuracy"] != -1 )
-          {
-            localMajor2.push(data.beacons[0]["accuracy"])
-            localTotal2++;
-          }
-          else if (data.beacons[1] !== undefined && data.beacons[1]["major"] == 2 && data.beacons[1]["accuracy"] !== undefined && data.beacons[1]["accuracy"] != -1 )
-          {
-            localMajor2.push(data.beacons[1]["accuracy"])
-            localTotal2++;
-          }
-          else if (data.beacons[2] !== undefined && data.beacons[2]["major"] == 2 && data.beacons[2]["accuracy"] !== undefined && data.beacons[2]["accuracy"] != -1 )
-          {
-            localMajor2.push(data.beacons[2]["accuracy"])
-            localTotal2++;
-          }
-          else
-            localMajor2.push(0)
-
-          // major 3 is either in element 0 or 1 or 2
-          if (data.beacons[0] !== undefined && data.beacons[0]["major"] == 3 && data.beacons[0]["accuracy"] !== undefined && data.beacons[0]["accuracy"] != -1 )
-          {
-            localMajor3.push(data.beacons[0]["accuracy"])
-            localTotal3++;
-          }
-          else if (data.beacons[1] !== undefined && data.beacons[1]["major"] == 3 && data.beacons[1]["accuracy"] !== undefined && data.beacons[1]["accuracy"] != -1 )
-          {
-            localMajor3.push(data.beacons[1]["accuracy"])
-            localTotal3++;
-          }
-          else if (data.beacons[2] !== undefined && data.beacons[2]["major"] == 3 && data.beacons[2]["accuracy"] !== undefined && data.beacons[2]["accuracy"] != -1 )
-          {
-            localMajor3.push(data.beacons[2]["accuracy"])
-            localTotal3++;
-          }
-          else
-            localMajor3.push(0)
-
-          this.setState({count: localCount, major1: localMajor1, major2: localMajor2, major3: localMajor3, total1: localTotal1, total2: localTotal2, total3: localTotal3});
-
+        // take a closer look at the cloest beacon (aka first elem after sorting data.beacons)
+        // if accurancy of the closest beacon is less than the cutoff, push 1; otherwise push 0
+        // 1 means in-range; 0 means out-of-range
+        if (data.beacons[0]["accuracy"] <= cutoff[ data.beacons[0]["major"] ]) {
+          roomDict[ data.beacons[0]["major"] ].push(1)
         }
         else {
-          let localAverage1 = 0;
-          let localAverage2 = 0;
-          let localAverage3 = 0;
-
-          if (localTotal1 == 0)
-            localAverage1 = 0;
-          else
-            localAverage1 = localMajor1.reduce((a, b) => a + b, 0) / localTotal1;
-
-          if (localTotal2 == 0)
-            localAverage2 = 0;
-          else
-            localAverage2 = localMajor2.reduce((a, b) => a + b, 0) / localTotal2;
-
-          if (localTotal3 == 0) 
-            localAverage3 = 0;
-          else 
-            localAverage3 = localMajor3.reduce((a, b) => a + b, 0) / localTotal3;
-
-        if (localAverage1 <= 1 && localAverage1 != 0) {
-            this.updateDoctorLocation(phoneNumber, 'exam1');
-        } 
-        else if (localAverage2 <= 1 && localAverage2 != 0) {
-            this.updateDoctorLocation(phoneNumber, 'CTRoom');
+          roomDict[ data.beacons[0]["major"] ].push(0)
         }
-        else if (localAverage3 <= 1 && localAverage3 != 0) {
-          this.updateDoctorLocation(phoneNumber, 'femaleWaitingRoom');
+        // for any other beacons which is not the closest, push 0 (out-of-range)
+        for (let i = 1; i < data.beacons.length; i++) {
+          roomDict[ data.beacons[i]["major"] ].push(0)
         }
-        else 
-            this.updateDoctorLocation(phoneNumber, 'Private')
-
-          this.setState({count: 0, major1: [], major2: [], major3: [], total1: 0, total2: 0, total3: 0, average1: localAverage1, average2: localAverage2, average3: localAverage3});
+        // after 10 rounds, implement FIFO
+        if (localCount >= 10) {
+          // pop out the oldest beacon data at front of the array
+          for (let beacon of data.beacons) {
+            roomDict[ beacon["major"] ].shift()
+          }
+          let hasAllOnes = false;
+          // check which beacon major's array has all 1's (in-range the whole time)
+          // set the currRoom; hasAllOnes = true
+          for (let beacon of data.beacons) {
+            if (roomDict[ beacon["major"] ].every((val) => val === 1)) {
+              this.setState({currRoom: majorToRoom[ beacon["major"] ]});
+              hasAllOnes = true;
+              break;
+            }
+          }
+          // if no array has all 1's, set currRoom to Private
+          if (!hasAllOnes) {
+            this.setState({currRoom: 'Private'});
+          }
         }
 
         this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(data.beacons)
+          dataSource: this.state.dataSource.cloneWithRows(data.beacons),
+          roomDict: roomDict
         });
+        // update the room val to Firebase; change dots on the map
+        this.updateDoctorLocation(phoneNumber, this.state.currRoom);
       }
     );
   }
@@ -183,8 +121,6 @@ export default class beacons extends Component<Props> {
   updateDoctorLocation(phoneNumber, roomId) {
     firebase.database().ref('/DoctorLocation/' + phoneNumber).update({
         room: roomId
-    }).then((data) => {
-        this.setState({ room: roomId })
     });
   }
 
@@ -218,36 +154,19 @@ export default class beacons extends Component<Props> {
   }
 
   render() {
-    const { bluetoothState, dataSource, major1, major2, major3, count, room, average1, average2, average3 } =  this.state;
+    const { bluetoothState, dataSource, count, currRoom, roomDict } =  this.state;
 
     return (
       <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.headline}>
           All beacons in the area will be displayed
         </Text>
-        <Text style={{fontWeight: 'bold'}}>Major 1:</Text>
-        {
-          major1.map((item, key)=>(
-            <Text key={key}> {item.toFixed(2)}m </Text>
-          ))
-        }
-        <Text style={{fontWeight: 'bold'}}>Major 2:</Text>
-        {
-          major2.map((item, key)=>(
-            <Text key={key}> {item.toFixed(2)}m </Text>
-          ))
-        }
-        <Text style={{fontWeight: 'bold'}}>Major 3:</Text>
-        {
-          major3.map((item, key)=>(
-            <Text key={key}> {item.toFixed(2)}m </Text>
-          ))
-        }
+        <Text>{roomDict[1]}</Text>
+        <Text>{roomDict[2]}</Text>
+        <Text>{roomDict[3]}</Text>
+        
         <Text style={{fontWeight: 'bold'}}>Count: {count} </Text> 
-        <Text style={{fontWeight: 'bold'}}> Average distance of major 1 (exam1): {average1.toFixed(2)}m </Text>
-        <Text style={{fontWeight: 'bold'}}> Average distance of major 2 (CTRoom): {average2.toFixed(2)}m </Text>
-        <Text style={{fontWeight: 'bold'}}> Average distance of major 3 (femaleWaitingRoom): {average3.toFixed(2)}m </Text>
-        <Text style={{fontWeight: 'bold', color: 'blue'}}> You are now in Room {room} </Text>
+        <Text style={{fontWeight: 'bold', color: 'blue'}}> You are now in Room {currRoom} </Text>
         <ListView
           dataSource={ dataSource }
           enableEmptySections={ true }
